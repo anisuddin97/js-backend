785314d88529a34d88f5e096b1111e67ed843968
SK Anisuddin Afridi <anisuddin.afridi@omnifi.in>
2024-04-14 12:30:39 +0530

    Error handeling and response classes done

--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit:"16kb"})) // we are going to acpet from req json who's limit 16kb
-app.use(express.urlencoded({extended:true, limit:"16kb"})) //urlencoded means we are going to acpet data throw url
-app.use(express.static("public")) // if i get any document from user then i will store that in public folder 
-app.use(cookieParser()) // then we will able to do CRUD operation on cookie
+app.use(express.json({limit:"16kb"}))    // we are going to acpet from req json who's limit 16kb
+app.use(express.urlencoded({extended:true, limit:"16kb"}))   //urlencoded means we are going to acpet data throw url
+app.use(express.static("public"))    // if i get any document from user then i will store that in public folder 
+app.use(cookieParser())      // then we will able to do CRUD operation on cookie
 
 
 export { app } 
